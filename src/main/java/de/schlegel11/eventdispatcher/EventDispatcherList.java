0c0278e8f2e7d7844161ef49c0121812515a4fc6
Marcel Schlegel <marcel.schlegel@posteo.de>
2014-11-09 00:00:40 +0100

    Intern modification

--- src/main/java/de/schlegel11/eventdispatcher/EventDispatcherList.java
+++ src/main/java/de/schlegel11/eventdispatcher/EventDispatcherList.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Sets;
 
 import java.util.EventListener;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 /**
  * Created by schlegel11 on 20.10.14.
@@ -13,22 +15,40 @@ import java.util.function.Consumer;
 final class EventDispatcherList {
 
     public static final EventDispatcherList PSEUDO_EMPTY_DISPATCHER_LIST = new EventDispatcherList();
-    private final Set<EventListener> listeners = Sets.newHashSet();
+    private final Set<EventListenerWrapper> listeners = Sets.newHashSet();
 
-    public boolean addListener(EventListener listener) {
-        return listeners.add(listener);
+    public boolean addListener(EventListener listener, int numberOfCalls) {
+        return listeners.add(new EventListenerWrapper(listener, numberOfCalls));
     }
 
     public boolean removeListener(EventListener listener) {
-        return listeners.remove(listener);
+        return listeners.removeIf(elw -> elw.getListener() == listener);
     }
 
     public int getListenerCount() {
         return listeners.size();
     }
 
+    public int getListenerMaxCalls(EventListener listener) {
+        EventListenerWrapper elw = getElw(listener);
+        return Objects.isNull(elw) ? 0 : elw.getMaxCalls();
+    }
+
+    public int getListenerCurrentCalls(EventListener listener) {
+        EventListenerWrapper elw = getElw(listener);
+        return Objects.isNull(elw) ? 0 : elw.getCurrentCalls();
+    }
+
+    public boolean isEmpty() {
+        return listeners.isEmpty();
+    }
+
     public void fireEvent(Consumer<EventListener> consumer) {
-        listeners.stream().forEach(consumer);
+        for (EventListenerWrapper elw : listeners) {
+            consumer.accept(elw.getListener());
+            elw.addCurrentCalls(elw.getMaxCalls() == EventListenerWrapper.INFINITE_CALLS ? 0 : 1);
+        }
+        listeners.removeIf(elw -> elw.getMaxCalls() == elw.getCurrentCalls());
     }
 
     @Override
@@ -46,5 +66,11 @@ final class EventDispatcherList {
         }
         final EventDispatcherList other = (EventDispatcherList) obj;
         return Objects.equals(this.listeners, other.listeners);
+    }
+
+    private EventListenerWrapper getElw(EventListener listener) {
+        Stream<EventListenerWrapper> stream = listeners.stream().filter(elw -> elw.getListener() == listener);
+        Optional<EventListenerWrapper> optional = stream.findFirst();
+        return optional.isPresent() ? optional.get() : null;
     }
 }
