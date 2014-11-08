c8e2a233402d05d98f58e98071e29fab86eec530
Marcel Schlegel <marcel.schlegel@posteo.de>
2014-11-09 00:01:24 +0100

    Add new test methods

--- src/test/java/de/schlegel11/eventdispatcher/EventDispatcherTest.groovy
+++ src/test/java/de/schlegel11/eventdispatcher/EventDispatcherTest.groovy
@@ -42,6 +42,50 @@ class EventDispatcherTest extends Specification {
         ex.message == "Argument is null."
     }
 
+    def "AddListener(maxCalls)Positive"() {
+        given:
+        def testListener = Mock(TestListener)
+
+        expect:
+        eventDispatcher.addListener(EventListener, listener, 5) == true
+        eventDispatcher.addListener(TestListener, testListener, -1) == true
+    }
+
+    def "AddListener(maxCalls)Negative"() {
+        given:
+        NullPointerException ex
+        IllegalArgumentException iae
+        eventDispatcher.addListener(EventListener, listener, 5)
+
+        expect:
+        eventDispatcher.addListener(EventListener, listener, 5) == false
+        eventDispatcher.addListener(EventListener, listener, 6) == false
+
+        when:
+        eventDispatcher.addListener(null, listener, 5)
+        then:
+        ex = thrown()
+        ex.message == "Argument is null."
+
+        when:
+        eventDispatcher.addListener(EventListener, null, 5)
+        then:
+        ex = thrown()
+        ex.message == "Argument is null."
+
+        when:
+        eventDispatcher.addListener(EventListener, listener, 0)
+        then:
+        iae = thrown()
+        iae.message == "Argument is less or equal 0 (except -1)."
+
+        when:
+        eventDispatcher.addListener(EventListener, listener, -2)
+        then:
+        iae = thrown()
+        iae.message == "Argument is less or equal 0 (except -1)."
+    }
+
     def "RemoveListenerPositive"() {
         given:
         eventDispatcher.addListener(EventListener, listener)
@@ -143,26 +187,87 @@ class EventDispatcherTest extends Specification {
     def "GetListenerTypeCount"() {
         given:
         def anotherListener = Mock(EventListener)
+        def testListener = Mock(TestListener)
         def emptyEventDispatcher = EventDispatcher.newInstance()
+        eventDispatcher.addListener(EventListener, listener)
+        eventDispatcher.addListener(EventListener, anotherListener)
+        eventDispatcher.addListener(Object, anotherListener)
+        eventDispatcher.addListener(TestListener, testListener)
+
+        expect:
+        eventDispatcher.getListenerTypeCount() == 3
+        eventDispatcher.removeListenerType(EventListener)
+        eventDispatcher.getListenerTypeCount() == 2
+        eventDispatcher.removeListenerType(Object)
+        eventDispatcher.getListenerTypeCount() == 1
+        eventDispatcher.removeListener(TestListener, testListener)
+        eventDispatcher.getListenerTypeCount() == 0
+        emptyEventDispatcher.getListenerTypeCount() == 0
+    }
+
+    def "GetListenerMaxCalls"() {
+        given:
+        def anotherListener = Mock(EventListener)
+        def emptyEventDispatcher = EventDispatcher.newInstance()
+        eventDispatcher.addListener(EventListener, listener, 15)
+        eventDispatcher.addListener(EventListener, anotherListener, 20)
+        eventDispatcher.addListener(Object, anotherListener, 5)
+
+        expect:
+        eventDispatcher.getListenerMaxCalls(EventListener, listener) == 15
+        eventDispatcher.getListenerMaxCalls(EventListener, anotherListener) == 20
+        eventDispatcher.getListenerMaxCalls(Object, anotherListener) == 5
+        eventDispatcher.getListenerMaxCalls(String, anotherListener) == 0
+        eventDispatcher.getListenerMaxCalls(Object, listener) == 0
+        emptyEventDispatcher.getListenerMaxCalls(EventListener, listener) == 0
+    }
+
+    def "GetListenerCurrentCalls"() {
+        given:
+        def anotherListener = Mock(EventListener)
+        def emptyEventDispatcher = EventDispatcher.newInstance()
+        eventDispatcher.addListener(EventListener, listener, 15)
+        eventDispatcher.addListener(EventListener, anotherListener, 20)
+        eventDispatcher.addListener(Object, anotherListener, 5)
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        eventDispatcher.fireEvent(Object, { l -> l.toString() })
+        eventDispatcher.fireEvent(Object, { l -> l.toString() })
+        eventDispatcher.fireEvent(Object, { l -> l.toString() })
+        eventDispatcher.fireEvent(Object, { l -> l.toString() })
+
+        expect:
+        eventDispatcher.getListenerCurrentCalls(EventListener, listener) == 4
+        eventDispatcher.getListenerCurrentCalls(EventListener, anotherListener) == 4
+        eventDispatcher.getListenerCurrentCalls(Object, anotherListener) == 4
+        eventDispatcher.fireEvent(Object, { l -> l.toString() })
+        eventDispatcher.getListenerCurrentCalls(Object, anotherListener) == 0
+        emptyEventDispatcher.getListenerCurrentCalls(Object, anotherListener) == 0
+    }
+
+    def "Clear"() {
+        given:
+        def anotherListener = Mock(EventListener)
         eventDispatcher.addListener(EventListener, listener)
         eventDispatcher.addListener(EventListener, anotherListener)
         eventDispatcher.addListener(Object, anotherListener)
 
         expect:
-        eventDispatcher.getListenerTypeCount() == 2
-        eventDispatcher.removeListenerType(EventListener)
-        eventDispatcher.getListenerTypeCount() == 1
-        eventDispatcher.removeListenerType(Object)
+        eventDispatcher.clear()
+        eventDispatcher.getListenerCount() == 0
         eventDispatcher.getListenerTypeCount() == 0
-        emptyEventDispatcher.getListenerTypeCount() == 0
     }
 
     def "FireEventPositive"() {
         given:
         def testListener = Mock(TestListener)
+        def listenerMaxCalls = Mock(EventListener)
         def eventObject = new EventObject("TestSource")
         def anotherEventObject = new EventObject("TestSource2")
         eventDispatcher.addListener(TestListener, testListener)
+        eventDispatcher.addListener(EventListener, listenerMaxCalls, 1)
 
         when:
         eventDispatcher.fireEvent(TestListener, { l -> l.testMethod(eventObject) })
@@ -178,19 +283,34 @@ class EventDispatcherTest extends Specification {
         expect:
         eventDispatcher.fireEvent(TestListener, { l -> l.testMethod(eventObject) }) == true
         eventDispatcher.fireEvent(TestListener, {}) == true
+
+        when:
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
+        then:
+        1 * listenerMaxCalls.toString()
+
+        expect:
+        eventDispatcher.getListenerCount() == 1
+        eventDispatcher.getListenerCount(EventListener) == 0
+        eventDispatcher.getListenerTypeCount() == 1
     }
 
     def "FireEventNegative"() {
         given:
         def testListener = Mock(TestListener)
+        def listenerMaxCalls = Mock(EventListener)
         def eventObject = new EventObject("TestSource")
         def emptyEventDispatcher = EventDispatcher.newInstance()
         eventDispatcher.addListener(TestListener, testListener)
+        eventDispatcher.addListener(EventListener, listenerMaxCalls, 1)
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() })
 
         expect:
         eventDispatcher.fireEvent(null, { l -> l.testMethod(eventObject) }) == false
         eventDispatcher.fireEvent(TestListener, null) == false
         eventDispatcher.fireEvent(Object, { l -> l.testMethod(eventObject) }) == false
         emptyEventDispatcher.fireEvent(Object, { l -> l.testMethod(eventObject) }) == false
+        eventDispatcher.fireEvent(EventListener, { l -> l.toString() }) == false
     }
 }
